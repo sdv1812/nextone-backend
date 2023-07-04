981139ac08f3d68eadd04848fc3e5de1f1725557
sanskar <sanskar.verma90@gmail.com>
2023-07-04 11:42:14 +1000

    Initialize

---
+++ src/main/java/com/vdv/NExTone/NExToneApplication.java
@@ -0,0 +1,13 @@
+package com.vdv.NExTone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NExToneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NExToneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vdv/NExTone/NExToneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdv.NExTone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NExToneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
